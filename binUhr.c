4d82d9852d25b67a379863cef9ed6219ba8edcbf
krapottke <krapottke_git@icloud.com>
2025-02-25 19:10:13 +0100

    tasks

---
+++ binUhr.c
@@ -0,0 +1,7 @@
+// TODO: Binäruhr mit Atmega mController realisieren
+// Aufbau nach selbstentworfenem Schaltplan mit selbst erstellter Platine
+
+
+// Binäruhr auf exterenen uhrenquarz als taktgeber 
+// Zählen per "Taktinterrupt" ansonsten sleepmode um Energie zu sparen
+// Zeitkorrektur nach messung im Labor
